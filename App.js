4cd4b8cff5f4c23228128939c4f975bc4f23365a
amarjahan <amarjahan900@gmail.com>
2023-06-13 11:51:10 +0530

    App

---
+++ App.js
@@ -0,0 +1,50 @@
+{
+    var x=20;
+}
+console.log(x); 
+
+{
+    let x=40;
+    console.log(x);
+}
+{
+    const last_name="holi";
+    window.prompt(last_name);
+}
+{
+    const x=10;
+    const y=20;
+    console.log(x);
+}   
+ {   
+    const button=document.querySelector("button");
+    button.addEventListener("click",
+        console.log("buttonclicked")
+    );
+}
+
+{
+    let x=90;
+    let y=10;
+    let z=x*y;
+    console.log(z);
+}
+
+{
+    var x= 10;
+    console.log(x +=5);
+}
+
+{
+    let result1=2;
+    let result2=8;
+    let result3=result1<=result2;
+    console.log(result3);
+}
+
+{
+    let result1=2;
+    let result2=8;
+    let result3=result1&result2;
+    console.log(result3);
+}
