6a5466c94402d8c5a589ff3d8db7a9b4626d47e5
amarjahan <amarjahan900@gmail.com>
2023-06-15 12:00:14 +0530

    15-6-23

--- App.js
+++ App.js
@@ -1,53 +1,102 @@
-{
-    var x=20;
-}
-console.log(x); 
+// {
+//     var x=20;
+// }
+// console.log(x); 
 
-{
-    let x=40;
-    console.log(x);
-}
-{
-    const last_name="holi";
-    window.prompt(last_name);
-}
-{
-    const x=10;
-    const y=20;
-    console.log(x);
-}   
- {   
-    const button=document.querySelector("button");
-    button.addEventListener("click",
-        console.log("buttonclicked")
-    );
-}
+// {
+//     let x=40;
+//     console.log(x);
+// }
 
-{
-    let x=90;
-    let y=10;
-    let z=x*y;
-    console.log(z);
-}
+// {
+//     const last_name="holi";
+//     window.prompt(last_name);
+// }
+// {
+//     const x=10;
+//     const y=20;
+//     console.log(x);
+// } 
 
-{
-    var x= 10;
-    console.log(x +=5);
-}
-{
-
-    let result1=2;
-    let result2=8;
-    let result3=result1<=result2;
-    console.log(result3);
-}
+//  {   
+//     const button=document.querySelector("button");
+//     button.addEventListener("click",
+//         console.log("buttonclicked")
+//     );
+// }
 
 
-    let result1=2;
-    let result2=8;
-let result3 = 10;
-console.log(result1 < result2 && result2 < result3);
-console.log(result1 < result2 && result2 > result3);
-console.log(result1 > result2 && result2 < result3);
-console.log(result1 > result2 && result2 > result3);
+// {
+//     let x=90;
+//     let y=10;
+//     let z=x*y;
+//     console.log(z);
+// }
 
+// {
+//     var x= 10;
+//     console.log(x +=5);
+// }
+// {
+
+//     let result1=2;
+//     let result2=8;
+//     let result3=result1<=result2;
+//     console.log(result3);
+// }
+
+
+//     let result1=2;
+//     let result2=8;
+// let result3 = 10;
+// console.log(result1 < result2 && result2 < result3);
+// console.log(result1 < result2 && result2 > result3);
+// console.log(result1 > result2 && result2 < result3);
+// console.log(result1 > result2 && result2 > result3);
+// 
+// let x="30";
+// let y=30;
+// console.log(x===y);
+
+// function web(){
+//      x=20;
+//      y=34;
+// };
+// web()
+// console.log(x*y);
+// console.log(x===y);
+
+// let x="";
+// console.log(typeof(x));
+// const obj={name:"janu",age:20}
+// console.log(obj);
+
+// const obj={name:"janu",age:20}
+// console.log(obj.name);
+
+
+
+// const clickBtn=document.querySelector(".clickbtn");
+// const headTag=document.querySelector(".hidehead");
+// clickBtn.addEventListener("click", function web() {
+//     return headTag.classList.toggle("hidehead-active");
+// });
+// web();
+
+
+// const clickBtn=document.querySelector(".clickbtn");
+// const headTag=document.querySelector(".hidehead");
+// clickBtn.addEventListener("click",function web(){
+//     return headTag.classList.toggle("hidehead-active");
+// });
+
+const headTag=document.querySelector(".hidehead");
+function web(){
+        return headTag.classList.toggle("hidehead-active");
+    };
+
+
+
+
+   
+    
